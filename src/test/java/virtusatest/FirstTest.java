9ab39c789d96af5bf5e42a8f3df88b887eec3116
santhoshissa <34296918+santhoshissa@users.noreply.github.com>
2024-08-07 13:17:38 +0530

    Update FirstTest.java

--- src/test/java/virtusatest/FirstTest.java
+++ src/test/java/virtusatest/FirstTest.java
@@ -74,4 +74,35 @@ public class FirstTest {
 		driver.close();
 	}
 
+	@Test
+	public void thirdTestCase() {
+		
+		ChromeOptions options = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		options.addArguments("--remote-allow-orgins=*");
+		options.addArguments("--headless");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--ignore-ssl-errors=yes");
+		options.addArguments("--ignore-certificate-errors");
+		// options.setExperimentalOption("prefs", chromePrefs);
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-gpu");
+		WebDriver driver = new ChromeDriver(options);
+		
+		System.out.println("Launching Connect Google");
+		
+		driver.get("https://dev.connect.nonprod.gcpdnb.net/idaas-login");
+		Assert.assertEquals("D&B Connect", driver.getTitle());
+		
+		
+		// WebElement element=driver.findElement(By.name("q"));
+		
+		// element.sendKeys("Selenium Java");
+		
+		// element.submit();
+		// Assert.assertEquals("Selenium Java - Google Search", driver.getTitle());
+		driver.close();
+	}
+
 }

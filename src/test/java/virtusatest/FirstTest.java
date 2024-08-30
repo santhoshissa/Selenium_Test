54beeda6370a33ff1fdfb19172e08ac8ed00d8ca
santhoshissa <34296918+santhoshissa@users.noreply.github.com>
2024-08-30 18:47:48 +0530

    Update FirstTest.java

--- src/test/java/virtusatest/FirstTest.java
+++ src/test/java/virtusatest/FirstTest.java
@@ -100,6 +100,10 @@ public class FirstTest {
 		
 		
 		System.out.println("Launching Connect Google");
+		driver.get("https://stg.sso.dnb.com/app/dnb-stg_dnbconnectqagcve_1/exke2qihwzs6GPER21d7/sso/saml?SAMLRequest=nVNNj9owEP0rke%2BJExbxYQEShe0WiUIEbA%2B9IGMPYG1iB3sCdH99nQBdDl1U9WRr5s3Me8%2FjnuN5VrBhiXu9gEMJDoNznmnH6kSflFYzw51yTPMcHEPBlsPvU9aIYlZYg0aYjNyVPK7gzoFFZTQJJuM%2Bmc%2Bep%2FOXyWydbDtJJ97KsCniVtiEVjvsNjYQxu1EyO4mljyRJPgB1vnaPvGtfAPnSphoh1yjD8WNZhh3wqd4lTyxJGZJ9ycJxl6P0hzrqj1i4RilDneRcyaSehMJk1NeFNTfQx9f%2B1MYrUHgge%2FEEdYJhfMbNA5qf3p3rZf0edFIZJv6elrJJUF6NeGL0lLp3WP9mwvIsW%2BrVRqm8%2BWKBMObJyOjXZmDXYI9KgGvi%2BkHZwnH6Eos0kZ752W0E0UlQQPSInMUNIItrHJQkePCUQ9Yo3dgLf3FvCEng15FmtXW2cF%2FN88BueTIe%2FS%2BXe%2BySzOvejJOTabEr%2BCrsTnHz01JoqSOKBluayiDnKtsKKX1k7w5WWZOIwscoU%2FQlkACeht03VeQ9fZ69xDOGIxMXnBPtHpxOHOBN9H3qFHmN3EB28HDZRVMVDgf%2FkdY6o%2BTsfLqy18nXnKfsP%2BTvf%2BQg98%3D&RelayState=https%3A%2F%2Fdev.connect.nonprod.gcpdnb.net%2F");
+	    // driver.manage().window().setSize(new Dimension(1226, 741));
+	    driver.findElement(By.id("input28")).sendKeys("dcpqa_auto_ba01@dnb.mailinator.com");
+	    driver.findElement(By.cssSelector(".button")).click();
 		
 		// driver.get("https://dev.connect.nonprod.gcpdnb.net/");
 		// Assert.assertEquals("dnb-stg - Sign In", driver.getTitle());
@@ -113,8 +117,8 @@ public class FirstTest {
 		
 		// button.click();
 
-		driver.get("https://dev.connect.nonprod.gcpdnb.net/");
-		Assert.assertEquals("dnb-stg - Sign In", driver.getTitle());
+		// driver.get("https://dev.connect.nonprod.gcpdnb.net/");
+		// Assert.assertEquals("dnb-stg - Sign In", driver.getTitle());
 		
 		
 		// WebElement element=driver.findElement(By.id("input28"));
